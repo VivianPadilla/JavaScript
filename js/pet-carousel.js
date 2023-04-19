15176036a039555c0688a245784be78f6ec5abd2
Daniela estrada <estradaniela97@gmail.com>
2023-04-18 20:07:29 -0500

    json pets

--- js/pet-carousel.js
+++ js/pet-carousel.js
@@ -1,50 +1,6 @@
-const petsCarousel = [
-  {
-    id: 7,
-    imagen:
-      "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61TUUBnBDnS.jpg",
-    raza: "Beagle",
-    nombre: "Kira",
-    tamano: "Cachorro",
-    precio: 0,
-  },
-  {
-    id: 8,
-    imagen:
-      "https://i.pinimg.com/originals/13/93/f6/1393f645f31a0e0a807efbc1ab10c8de.jpg",
-    raza: "Pastor Alemán",
-    nombre: "Rosa",
-    tamano: "Cachorro",
-    precio: 0,
-  },
-  {
-    id: 9,
-    imagen:
-      "https://images.squarespace-cdn.com/content/v1/568988da0e4c11c934a13f25/addbb171-753c-4b6a-96e0-17852ed40ca0/2b214f6d-b3a5-40d1-a939-91aa8884ef8f.jpg",
-    raza: "Cocker",
-    nombre: "Muñeca",
-    tamano: "Adulto",
-    precio: 0,
-  },
-  {
-    id: 10,
-    imagen:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/bulldog-frances-1536054163.jpg?crop=1.00xw:0.834xh;0,0.0753xh&resize=980:*",
-    raza: "Bulldog francés",
-    nombre: "Firulais",
-    tamano: "Cachorro",
-    precio: 0,
-  },
-  {
-    id: 11,
-    imagen:
-      "https://i.pinimg.com/564x/fb/6e/02/fb6e027359f4a9fd3fa8438922d74a4d.jpg",
-    raza: "Bulldog",
-    nombre: "Eren",
-    tamano: "Cachorro",
-    precio: 0,
-  },
-];
+async function obtenerPetsCarousel(){
+  const resp = await fetch('/json/pets.json');
+  const petListCarousel = await resp.json();
 
 function generateCard(id, imagen, raza, nombre, tamano, precio) {
   return `
@@ -84,7 +40,7 @@ function popularPet(imagen, nombre, precio) {
 let petsCarouselList = "";
 let petPopularList = "";
 
-for (let pet of petsCarousel) {
+for (let pet of petListCarousel) {
   petsCarouselList += generateCard(
     pet.id,
     pet.imagen,
@@ -100,6 +56,9 @@ for (let pet of petsCarousel) {
   petPopularList += popularPet(pet.imagen, pet.nombre, pet.precio);
 }
 document.getElementById("popular_pet").innerHTML = petPopularList;
+}
+
+obtenerPetsCarousel();
 
 // let countCarousel = 0;
 
