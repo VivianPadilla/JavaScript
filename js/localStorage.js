6145447628ef72f9957136c3a9792466e58719bb
Daniela estrada <estradaniela97@gmail.com>
2023-04-19 00:07:24 -0500

    Local Storage

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,19 +1,10 @@
 
 
-const data = JSON.parse(localStorage.getItem("carrito"), 2);
+const data = (localStorage.getItem("carrito"));
 
 Swal.fire({
     title: '<strong>Carrito</u></strong>',
     icon: 'info',
     html:
         data,
-    showCloseButton: true,
-    showCancelButton: true,
-    focusConfirm: false,
-    confirmButtonText:
-        '<i class="fa fa-thumbs-up"></i> Great!',
-    confirmButtonAriaLabel: 'Thumbs up, great!',
-    cancelButtonText:
-        '<i class="fa fa-thumbs-down">[</i>',
-    cancelButtonAriaLabel: 'Thumbs down'
 })
